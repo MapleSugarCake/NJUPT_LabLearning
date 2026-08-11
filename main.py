734ed0d0552d3696843c184ab5ed26277213ae63
maplecake <maplecake@qq.com>
2026-08-11 16:34:00 +0800

    fix bugs

--- main.py
+++ main.py
@@ -3,4 +3,10 @@
 from labpass.cli import entrypoint
 
 if __name__ == "__main__":
+    print("script by MapleCake NJUPT2025管院新生")
+    print("    本脚本坚持免费，请抵制倒买倒卖行为")
+    print("    喵~管用贴吧给个好评喵~")
+    print("    github给个小星星就最好啦:https://github.com/MapleSugarCake/LabLearningAutoPass")
+    print("本脚本尚不完善，本脚本只可完成课程学习和课程答题，考试仍需自行手动完成。")
+    print("若脚本报错，请再白天换个时间重试，夜晚你邮服务器会宕机")
     entrypoint()
